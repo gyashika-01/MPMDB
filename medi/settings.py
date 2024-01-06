90de2abd21dff6eaa6bb8e7d311e3aa57c0822e7
gyashika-01 <gyashika11@gmail.com>
2024-01-06 13:19:59 +0530

    LOLZ

--- medi/settings.py
+++ medi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-wc!bkp6gr0p)rc&s1)jkmu+!^1v$=z%lha20h692%25l&tg+j0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', ' ', ' ']
+ALLOWED_HOSTS = ['.vercel.app', ' ', 'mpmdb.onrender.com']
 
 
 # Application definition
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
+MEDIA_URL = 'media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
